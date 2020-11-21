6e57880933ab11c5644e00d53a52b8594ffb1b97
adrian mendez <eladri-@live.com>
2020-11-20 20:17:17 -0400

    Add project files.

---
+++ BlazorServerLogin/Models/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerLogin.Models {
+    public class Estudiante {
+
+        [Key]
+        public int EstudianteId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Nombres { get; set; }
+
+        [Range(minimum: 1 , maximum: 10 , ErrorMessage = "Seleccione un semestre")]
+        public int Semestre { get; set; }
+
+
+        public string Nacionalidad { get; set; }
+
+    }
+}
