a551d03b444136fe188971d525d77af02a11ee0f
NiurbisReyesJaquez <71194774+NiurbisReyesJaquez@users.noreply.github.com>
2020-11-25 19:10:39 -0400

    Integrando los requerimientos a vehiculo

--- BlazorRentCar/Models/Vehiculo.cs
+++ BlazorRentCar/Models/Vehiculo.cs
@@ -10,22 +10,38 @@ namespace BlazorRentCar.Models {
     {
         [Key]
         public int VehiculoId { get; set; }
+
         public int ImportadorId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la matricula")]
         public string Matricula { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la placa")]
         public string Placa { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la marca")]
         public string Marca { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el modelo")]
         public string Modelo { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el Año de fabricacion")]
         public DateTime AñoFabricacion { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el estado del vehiculo")]
         public VehiculoEstado Estado { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el precio")]
         public decimal PrecioDia { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el kilometraje")]
         public int Kilometraje { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el Año del vehiculo")]
         public int Ano { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el chassis")]
         public string Chassis { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la cantidad de pasajeros")]
         public string Pasajeros { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la cantidad de puertas")]
         public string Puertas { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la traccion del vehiculo")]
         public string Traccion { get; set; }
         public string Comentario { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el valor del vehiculo")]
         public decimal Valor { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el tipo")]
         public string Tipo { get; set; }
 
         public Vehiculo()
